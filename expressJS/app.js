49b863c20b9c4d44b5e74bda49c321673145be7c
Mr. T <davidtr60@gmail.com>
2022-05-23 21:05:53 -0500

    Half down til 9

--- expressJS/app.js
+++ expressJS/app.js
@@ -12,10 +12,12 @@ const errorController = require('./controllers/error')
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
+
 app.use('/admin', adminRoutes)
+
 app.use(shopRoutes)
 
 app.use(errorController.get404)
